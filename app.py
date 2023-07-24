4e5e1bda7f3a37b8b89596bfced4aa6298c614d4
genxiaong <yunanxd1@gmail.com>
2023-07-24 20:51:18 +0800

    add package requirements and cors origin for API

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import (
         Flask,
         request
-        )
+    )
+from flask_cors import CORS
 from yahoo_image import search
 
 HTTP_OK = 200
 HTTP_ERR = 404
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/", methods=["GET"])
 def welcome():
